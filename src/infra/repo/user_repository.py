98db5221c92dc6d03234dc7ee708ddc3c503327f
joliveira <julio.oliveira@agriness.com>
2022-11-14 08:34:33 -0300

    feat: refactoring models in domain layer

--- src/infra/repo/user_repository.py
+++ src/infra/repo/user_repository.py
@@ -1,6 +1,6 @@
-from collections import namedtuple
+from src.domain.models import Users
 from src.infra.config import DBConnectionHandler
-from src.infra.entities import Users
+from src.infra.entities import Users as UsersModel
 
 class userRepository:
     """Class to manage User Repository"""
@@ -12,13 +12,13 @@ class userRepository:
                    - password: user password
            :return - tuple with new user inserted
         """
-        InsertData = namedtuple("Users", "id, name, password")
+
         with DBConnectionHandler() as db_connection:
             try:
-                new_user = Users(name=name, password=password)
+                new_user = UsersModel(name=name, password=password)
                 db_connection.session.add(new_user)
                 db_connection.session.commit()
-                return InsertData(id=new_user.id,name=new_user.name, password=new_user.password)
+                return Users(id=new_user.id,name=new_user.name, password=new_user.password)
             except:
                 db_connection.session.rollback()
                 raise
