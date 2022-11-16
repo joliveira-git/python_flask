f5fb519020efa39f6f23da642d4932e5ba15c6e4
joliveira <julio.oliveira@agriness.com>
2022-11-16 10:24:46 -0300

    feat: implementing select_pet in PetRepository

---
+++ src/data/interfaces/pet_repository_interface.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+from typing import List
+from src.domain.models import Pets
+
+class PetRepositoryInterface(ABC):
+    """Interface to pet repository"""
+
+    def in
