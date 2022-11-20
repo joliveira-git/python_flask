f2900d61070b48196761c183cd86e876254583cd
joliveira <julio.oliveira@agriness.com>
2022-11-20 11:12:51 -0300

    imp: implementing register_user route

--- src/presenters/errors/http_errors.py
+++ src/presenters/errors/http_errors.py
@@ -7,6 +7,11 @@ class HttpErrors:
         return {"status_code": 400, "body": {"error": "Bad request"}}
 
     @staticmethod
+    def error_409():
+        """HTTP 409"""
+        return {"status_code": 409, "body": {"error": "Conflict"}}
+
+    @staticmethod
     def error_422():
         """HTTP 422"""
         return {"status_code": 422, "body": {"error": "Unprocessable entity"}}
