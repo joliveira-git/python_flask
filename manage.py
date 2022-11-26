6bd1fa80be6a84d0faf2a14e11519c1549d18c41
joliveira <julio.oliveira@agriness.com>
2022-11-26 19:47:47 -0300

    requirements

--- manage.py
+++ manage.py
@@ -10,12 +10,12 @@ cli = FlaskGroup(create_app=create_app)
 with app.app_context():
     db.create_all()
 
+# TODO: Solve: Error: No such command 'recreate_db'.
 @cli.command("recreate_db")
 def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
-
 
 if __name__ == "__main__":
     cli()
