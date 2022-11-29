d1105d19e537ed8141d8a2dca56e0100eac5dfc0
joliveira <julio.oliveira@agriness.com>
2022-11-29 09:29:44 -0300

    feat: includes pet endpoint

--- src/infra/repo/pet_repository.py
+++ src/infra/repo/pet_repository.py
@@ -1,4 +1,6 @@
 from typing import List
+
+from src import db
 from src.data.interfaces import PetRepositoryInterface
 from src.domain.models import Pets
 from src.infra.config import DBConnectionHandler
@@ -18,24 +20,22 @@ class PetRepository(PetRepositoryInterface):
         :return tuple with new pet inserted
         """
 
-        with DBConnectionHandler() as db_connection:
-            try:
-                new_pet = PetsEntity(name=name, specie=specie, age=age, user_id=user_id)
-                db_connection.session.add(new_pet)
-                db_connection.session.commit()
 
-                return Pets(
-                    id=new_pet.id,
-                    name=new_pet.name,
-                    specie=new_pet.specie.value,
-                    age=new_pet.age,
-                    user_id=new_pet.user_id
-                )
-            except:
-                db_connection.session.rollback()
-                raise
-            finally:
-                db_connection.session.close()
+        try:
+            new_pet = PetsEntity(name=name, specie=specie, age=age, user_id=user_id)
+            db.session.add(new_pet)
+            db.session.commit()
+
+            return Pets(
+                id=new_pet.id,
+                name=new_pet.name,
+                specie=new_pet.specie.value,
+                age=new_pet.age,
+                user_id=new_pet.user_id
+            )
+        except:
+            db.session.rollback()
+            raise
 
         return None
 
@@ -56,14 +56,10 @@ class PetRepository(PetRepositoryInterface):
                 filters['user_id'] = user_id
 
             if filters:
-                with DBConnectionHandler() as db_connection:
-                    data = db_connection.session.query(PetsEntity).filter_by(**filters).all()
-                    query_data = [data]
+                data = db.session.query(PetsEntity).filter_by(**filters).all()
+                query_data = [data]
             return query_data
         except:
-            db_connection.session.rollback()
+            db.session.rollback()
             raise
-        finally:
-            if db_connection:
-                db_connection.session.close()
         return None
